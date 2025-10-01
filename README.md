5c10b963de6725c5c388daeb422347cccd68a07b
- <->
2025-10-01 19:20:32 +0200

    updated readme

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ git clone https://github.com/daniele47/nvim-lsp-pluggable "$HOME/.local/share/nv
 ```sh
 rm -rf "$HOME/.local/share/nvim/site"
 ```
+
+# usage
+
+- install lsp packages in `package/data`
+- link lsp packages to `package/bin`, to have it be included in neovim PATH
+- for each lsp, enable it in `plugin/lsp_enable.lua`
+- if lsp config file in `lsp` directory is missing, download it from `https://github.com/neovim/nvim-lspconfig`
--- cleanup_and_init.sh
+++ cleanup_and_init.sh
@@ -16,13 +16,3 @@ rm -rf "$SCRIPT_DIR/packages" "$SCRIPT_DIR/plugin/lsp_enable.lua"
 echo "creating new directories and files for lsp..."
 mkdir "$SCRIPT_DIR"/packages/{data,bin}/ -p
 echo "-- write here your vim.lsp.enable(...) without worries: this file is ignored!" > "$SCRIPT_DIR/plugin/lsp_enable.lua"
-
-# final message
-echo "
-INSTALLATION SUCCESSFULLY COMPLETED!
-
-How to interact:
-- install lsp packages in ./package/data
-- link lsp packages to ./package/bin/, to have it be included in neovim PATH
-- for each lsp, enable it in ./plugin/lsp_enable.lua
-"
